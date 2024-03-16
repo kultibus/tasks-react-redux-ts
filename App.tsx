49c85e275bf065fd3149c7e3c2ce9af0f0e072a3
Basil <bastumpo@gmail.com>
2024-03-16 14:00:28 +0800

    start dev

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import { useEffect, useState } from "react";
+
+export const App = () => {
+
+    return <div>app</div>;
+};
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshTypeScript = require("react-refresh-typescript");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+
+  output: {
+    filename: "[name].[contenthash:8].js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new ReactRefreshWebpackPlugin(),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[local]_[hash:base64:8]",
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+
+      {
+        test: /\.tsx?$/,
+
+        use: {
+          loader: "ts-loader",
+          options: {
+            getCustomTransformers: () => ({
+              before: [ReactRefreshTypeScript()],
+            }),
+
+            transpileOnly: true,
+          },
+        },
+        exclude: /node_modules/,
+      },
+
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+
+  devServer: {
+    port: 5000,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+  },
+};
